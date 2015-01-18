67a3914551172b3194f9fcff7685e4af5a564c77
Isaac Wycoff <isaac.wycoff@gmail.com>
2015-01-18 11:06:21 -0800

    FUCK YOU GIT HUB
    Just seriously. Die. Stab yourself in the stomach and die of sepsis.

---
+++ DerpinDragons/Game.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DerpinDragons
+{
+    class DerpinGame : Game
+    {
+        public DerpinGame()
+        {
+
+        }
+
+        protected virtual void Initialize()
+        {
+            new GraphicsDeviceManager(this);
+        }
+
+        protected virtual void LoadContent()
+        {
+            var spriteBatch = new SpriteBatch(this.GraphicsDevice);
+        }
+
+        protected virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        protected virtual void Draw(GameTime gameTime)
+        {
+
+
+        }
+
+        protected virtual void UnloadContent()
+        {
+
+        }
+
+
+    }
+}
